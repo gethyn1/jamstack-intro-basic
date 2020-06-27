2c0de112f025189aba12055bb7207183a37ab760
Gethyn Jones <gethyn1@gmail.com>
2020-06-27 11:08:37 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log('module')
+const listRepos = async (userName) => {
+  const repos = await fetch(
+    `https://api.github.com/users/${userName}/repos?type=owner&sort=updated`
+  )
+  .then(res => res.json())
+  .catch(error => console.error(error))
+
+  const markup = repos
+    .map(
+      (repo) => `
+        <li><a href="${repo.html_url}">${repo.name}</a>
+        (⭐️ ${repo.stargazers_count})
+      `)
+    .join('')
+
+  const content = document.getElementById('content')
+  content.innerHTML = `<ul>${markup}</ul>`
+}
+
+listRepos('gethyn1')
